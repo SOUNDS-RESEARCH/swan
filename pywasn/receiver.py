089f4d5a870c2e1f8cf7fd6d8514a22fdb85bd1d
Eric Grinstein <eric.grinstein@outlook.com>
2021-09-24 16:30:25 +0100

    Update interface

--- pywasn/receiver.py
+++ pywasn/receiver.py
@@ -1,17 +1,26 @@
+"""
+This file defines the MicrophoneReceiver class, which is able to connect to a 
+remote computer using a socket and play the signals coming from it.
+"""
+
+import argparse
 import pyaudio
 import socket
 
 from settings import (
-    FORMAT, CHANNELS, RATE, CHUNK, PORT, ADDRESS,
+    FORMAT, CHANNELS, RATE, CHUNK, PORT,
     SOCKET_ADDRESS_FAMILY, SOCKET_KIND
 )
 
 
 class MicrophoneReceiver:
-    def __init__(self):
+    def __init__(self, sender_address=None):
         # Initialize network
+        if not sender_address:
+            hostname = socket.gethostname()
+            sender_address = socket.gethostbyname(hostname)
         self.receiver_socket = socket.socket(SOCKET_ADDRESS_FAMILY, SOCKET_KIND)
-        self.receiver_socket.connect((ADDRESS, int(PORT)))
+        self.receiver_socket.connect((sender_address, int(PORT)))
         
         # Initialize audio (optional, for playback)
         self.audio = pyaudio.PyAudio()
@@ -36,16 +45,16 @@ class MicrophoneReceiver:
         self.audio.terminate()
 
 
-def main():
+def main(sender_address):
+    microphone_receiver = MicrophoneReceiver(sender_address)
 
-    microphone_receiver = MicrophoneReceiver()
-
-    try:
-        while True:
-            microphone_receiver.receive()
-    except KeyboardInterrupt:
-        pass
+    while True:
+        microphone_receiver.receive()
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser("Create client which receives microphone signals from a remote computer")
+    parser.add_argument("--sender_address", type=str)
+    args = parser.parse_args()
+
+    main(args.sender_address)
--- pywasn/sender.py
+++ pywasn/sender.py
@@ -1,20 +1,28 @@
+"""
+This file contains the MicrophoneSender class,
+which defines a server socket which accepts connections
+from other computers and sends them the audio signals recorded by it.
+""" 
+
+import argparse
 import pyaudio
 import socket
-# import select, selectors
-
 
 from settings import (
-    FORMAT, CHANNELS, RATE, CHUNK, ADDRESS, PORT,
+    FORMAT, CHANNELS, RATE, CHUNK, PORT,
     SOCKET_ADDRESS_FAMILY, SOCKET_KIND
 )
 
 
 class MicrophoneSender:
-    def __init__(self):
-
+    def __init__(self, address=None):
         # Initialize network
         self.server_socket = socket.socket(SOCKET_ADDRESS_FAMILY, SOCKET_KIND)
-        self.server_socket.bind((ADDRESS, PORT))
+        if not address:
+            hostname = socket.gethostname()
+            address = socket.gethostbyname(hostname)
+
+        self.server_socket.bind((address, PORT))
         self.server_socket.listen()
         #self.read_list = [self.server_socket]
         self.client_sockets = []
@@ -26,42 +34,29 @@ class MicrophoneSender:
                                       rate=RATE,
                                       input=True,
                                       frames_per_buffer=CHUNK,
-                                      stream_callback=self.callback)
+                                      stream_callback=self.send_new_audio_frame_to_clients)
         
         print("Recording...")
     
-    def listen_and_send(self):
-        #readable, writable, errored = select.select(self.read_list, [], [])
-
+    def add_client(self):
+        
+        # I think the following function blocks the entire code until a message is received
         client_socket, address = self.server_socket.accept()
-        #self.read_list.append(clientsocket)
-        print("Connection from", address)
+        print("Connected to", address)
         self.client_sockets.append(client_socket)
         
-        #     data = s.recv(1024)
-        #     if not data:
-        #         self.read_list.remove(s)
-        
-        # for s in readable:
-        #     if s is self.server_socket:
-        #         (clientsocket, address) = self.server_socket.accept()
-        #         self.read_list.append(clientsocket)
-        #         print("Connection from", address)
-        #     else:
-        #         data = s.recv(1024)
-        #         if not data:
-        #             self.read_list.remove(s)
-        
-    def callback(self, in_data, frame_count, time_info, status):
-        # for s in self.read_list[1:]:
-        #     s.send(in_data)
+    def send_new_audio_frame_to_clients(self, in_data, frame_count, time_info, status):
+        """This function is called every time a new audio frame is received.
+           It proceeds to send the frame to all clients connected to this node
+        """
+
         for client in self.client_sockets: 
             client.send(in_data)
         return (None, pyaudio.paContinue)
     
     def close(self):
         self.server_socket.close()
-        # stop Recording
+
         self.stream.stop_stream()
         self.stream.close()
         self.audio.terminate()
@@ -69,17 +64,22 @@ class MicrophoneSender:
         print("Finished recording")
 
 
-def main():
-    microphone_sender = MicrophoneSender()
+def main(address):
+    microphone_sender = MicrophoneSender(address)
 
     try:
         while True:
-            microphone_sender.listen_and_send()
+            microphone_sender.add_client()
 
     except KeyboardInterrupt:
         pass
 
     microphone_sender.close()
 
+
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser("Create server which sends microphone signals to a remote computer")
+    parser.add_argument("--address", type=str, help="Address of this server. Defaults to the machine's IP address")
+    args = parser.parse_args()
+
+    main(args.address)
