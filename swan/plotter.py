d99614ac2541a8b0d1ae711eb6268156ed71982d
Matthias Blochberger <matthias.blochberger@esat.kuleuven.be>
2023-02-07 17:41:56 +0100

    rms now plotted over time

--- swan/plotter.py
+++ swan/plotter.py
@@ -14,8 +14,15 @@ class Plotter:
             self.feature_data[feature_key] = {}
         # Currently only creating one plot, which
         # will hold the MSC.
-        self.fig, self.axs = plt.subplots(nrows=len(self.feature_keys))
-        self.axs.set_title("RMS of each device")
+        self.fig, axs = plt.subplots(nrows=len(self.feature_keys))
+        axs = axs if type(axs) == list else [axs]
+
+        self.axs = {}
+        for ax, key in zip(axs, self.feature_keys):
+            self.axs[key] = ax
+
+        self._init_rms()
+        # initialize the plots for features here
 
         plt.ion()
         plt.show()
@@ -29,24 +36,10 @@ class Plotter:
             and the values are dicts with keys being the feature names and
             values being their respective feature values.
         """
-        for feature_key in self.feature_keys:
-            self.feature_data[feature_key] = {}
-            for device_ip in self.device_ips.keys():
-                if device_ip in features and feature_key in features[device_ip]:
-                    self.feature_data[feature_key][device_ip] = features[device_ip][feature_key]
-                # else:
-                #     self.feature_data[feature_key][device_ip] = np.nan
 
-        # self._update_rms(features)
+        self._update_rms(features)
         # Add new features plotting functions here.
         # You may use the _update_rms function as a template.
-        if len(self.feature_keys) == 1:
-            self.axs.clear()
-            self.axs.bar(self.feature_data[feature_key].keys(), self.feature_data[self.feature_keys[0]].values())
-        else:
-            for id, feature_key in enumerate(self.feature_keys):
-                self.axs[id].clear()
-                self.axs[id].bar(self.feature_data[feature_key].keys(), self.feature_data[feature_key].values())
 
         self.fig.canvas.draw()
         # This will run the GUI event
@@ -54,16 +47,31 @@ class Plotter:
         # currently waiting have been processed
         self.fig.canvas.flush_events()
 
+    def _init_rms(self):
+        self.axs["rms"].set_title("RMS of each device")
+
     def _update_rms(self, features):
-        # device_ips = sorted(list(features.keys()))
-        # print(device_ips)
-        # for device_ip, features_ in features.items():
-        #     for feature_key, value in features_.items():
-        #         self.feature_data[feature_key][device_ip] = value
-        pass
+        RMS_FRAMES = 128
+        feature_key = "rms"
+        # update local data
+        
+        # maybe this can be abstracted, but it's very feature-dependent
+        # self.feature_data[feature_key] = {}
+        self.axs[feature_key].clear()
+        for device_ip in self.device_ips.keys():
+            if device_ip not in self.feature_data[feature_key]:
+                self.feature_data[feature_key][device_ip] = [0]*RMS_FRAMES
+            if device_ip in features and feature_key in features[device_ip]: # this might be slow, who knows, but it's just plotting, does it block the thread?
+                self.feature_data[feature_key][device_ip].append(features[device_ip][feature_key])
+                self.feature_data[feature_key][device_ip].pop(0)
+
+            self.axs[feature_key].plot(self.feature_data[feature_key][device_ip], label=device_ip)
+        
+        self.axs[feature_key].legend()
+
+        # update plots
 
     def update_ips(self, add, ip):
-        # print("pepe")
         if add == True:
             self.device_ips[ip] = True
         else:
